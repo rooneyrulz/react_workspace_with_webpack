e8f1bc8812f7e0a94675574ac8ddd97d663dbe49
rooneyrulz <roony0782045336@gmail.com>
2019-07-01 17:17:14 +0530

    css setup"

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   },
